d777bd7dc78ef97a1a412ce7ebab52e026abf37b
finnvankrieken <finn@demetrixbio.com>
2019-06-13 09:20:31 -0700

    moved shared to plot files

--- plot2.R
+++ plot2.R
@@ -1,4 +1,8 @@
-source("shared.R")
+library(dplyr)
+
+power_consumption <- read.table("household_power_consumption.txt", sep=";", na.strings = "?", stringsAsFactors = F, header = T) %>%
+  filter((as.Date("2007-02-01") <= as.Date(strptime(Date, "%d/%m/%Y"))) & (as.Date("2007-02-02") >= as.Date(strptime(Date, "%d/%m/%Y")))) %>%
+  mutate(datetime = as.POSIXct(strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S")))
 
 png(file="plot2.png")
 with(power_consumption, {
--- plot4.R
+++ plot4.R
@@ -1,4 +1,8 @@
-source("shared.R")
+library(dplyr)
+
+power_consumption <- read.table("household_power_consumption.txt", sep=";", na.strings = "?", stringsAsFactors = F, header = T) %>%
+  filter((as.Date("2007-02-01") <= as.Date(strptime(Date, "%d/%m/%Y"))) & (as.Date("2007-02-02") >= as.Date(strptime(Date, "%d/%m/%Y")))) %>%
+  mutate(datetime = as.POSIXct(strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S")))
 
 png(file="plot4.png")
 par(mfrow= c(2,2))
@@ -17,7 +21,8 @@ with(power_consumption, {
     "topright",
     legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
     col = c("Black", "Red", "Blue"), 
-    lty=c(1,1,1)
+    lty=c(1,1,1),
+    bty = "n"
   )
   
   plot(datetime, Global_reactive_power, type="n")
